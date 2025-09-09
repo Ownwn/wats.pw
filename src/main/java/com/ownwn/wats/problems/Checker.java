46f70495fcb61d4ee0f30b999ff22d62c6f39bbb
Owen <owen@ownwn.com>
2025-09-09 19:27:58 +1200

    Setup docker

--- src/main/java/com/ownwn/wats/problems/Checker.java
+++ src/main/java/com/ownwn/wats/problems/Checker.java
@@ -4,10 +4,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class Checker {
+    private static final AtomicLong lastRequest = new AtomicLong(0);
+
+
     @PostMapping("check")
     public String check(@RequestBody String input) {
-        return input.contains("key") ? "good!" : "fail :(";
+        if (System.currentTimeMillis() - lastRequest.get() < 2000L) {
+            return "STOP SPAMMING!!";
+        }
+        lastRequest.set(System.currentTimeMillis());
+        List<File> files = Arrays.stream(Path.of("dock").toAbsolutePath().toFile().listFiles()).toList();
+
+
+        return isGood(input) ? "Good!" : "Fail";
+    }
+
+    private boolean isGood(String input) {
+        return input.contains("key");
     }
 }
